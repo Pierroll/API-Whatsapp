6b5845753c176c264c02e984817fdae3f2809437
Pierol <pierol@MacBook-Air-2.local>
2025-09-29 02:11:50 -0500

    add/credencials

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,11 @@ module.exports = {
       env: {
         NODE_ENV: "production",
         PORT: "3002",
-        API_KEY: "22c746590447e7311801c22c4d53736d569843ebf6da3cf8498354399fe2f2e2"
+        API_KEY: "22c746590447e7311801c22c4d53736d569843ebf6da3cf8498354399fe2f2e2",
+                QR_LOGIN_USER: "wa-user-api",
+        QR_LOGIN_PASS: "rollingcode",
+        COOKIE_SECRET: "a4ae8dea65b7a6cbad1d2f74714321ceb6f7b11a2cee6d4b64881746a68cc225"
+        
       }
     }
   ]
